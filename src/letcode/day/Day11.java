37ddefa9d0bcd3da71556497fad33223bb173c6e
zbs <2829024592@qq.com>
2020-04-18 19:31:56 +0800

    盛最多水的容器

---
+++ src/letcode/day/Day11.java
@@ -0,0 +1,21 @@
+package letcode.day;
+
+/**
+ * @author zbs
+ *	盛最多水的容器
+ *	https://leetcode-cn.com/problems/container-with-most-water/
+ */
+public class Day11 {
+    public int maxArea(int[] height) {
+		int max = 0, i = 0, j = height.length-1;
+		while(i < j) {
+			max = Math.max(max, Math.min(height[i], height[j])*(j-i));
+			if(height[i] < height[j]) {
+				i++;
+			}else {
+				j--;
+			}
+		}
+		return max;
+    }
+}
